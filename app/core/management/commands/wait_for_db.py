1fbfb509b0da16e2f002a6dc4cb575a24524fb58
ValverdeVinicius <valverdevinicius@protonmail.com>
2022-12-16 10:59:18 -0300

    Mais uma vez

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,14 +14,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """ Ponto de entrada para o comando"""
-        self.stdout.write('Esperando pelo Banco de Dados...')
+        self.stdout.write
+        ('Esperando pelo Banco de Dados...')
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except(Psycopg2OpError, OperationalError):
-                self.stdout.write('Banco de dados indisponível, espere 1 segundo...')
-                time.sleep(1)
-                
+                self.stdout.write
+                ('Banco de dados indisponível, espere 1 segundo...')
+                time.sleep(1)      
         self.stdout.write(self.style.SUCCESS('Banco de Dados Disponível!'))
